d72c297002413aed99858cd0d39fe297a0ebed6e
unknown <christopherjesuraj678@gmail.com>
2026-03-20 00:36:18 +0530

    refinement

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,9 @@ import styles from './NavBar.module.css'
 export default function NavBar() {
   return (
     <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <img src="/rit-logo.png" alt="RIT Logo" className={styles.logo} />
+      </div>
       <div className={styles.glassContainer}>
         <a href="#hero" className={styles.link}>Home</a>
         <a href="#about" className={styles.link}>About</a>
